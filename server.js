741ddfa682f019b6685852efd453c9701cc1504d
ZelleM <groovyrlm@gmail.com>
2019-08-16 20:42:04 -0400

    Getting started

---
+++ server.js
@@ -0,0 +1,38 @@
+//8/24/19 homework
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hey Y'all");
+});
+
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
+});
