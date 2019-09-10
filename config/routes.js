db4e9a9461c87effd647eba29fcb558602f52c82
ZelleM <groovyrlm@gmail.com>
2019-09-10 17:53:32 -0400

    hooking up heroku

--- config/routes.js
+++ config/routes.js
@@ -12,7 +12,7 @@ console.log("line 10 routes.js")
 // If deployed, use the deployed database. Otherwise use the local database
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articlescraper";
 //mongoose.connect(MONGODB_URI);
-mongoose.connect("mongodb://localhost/articlescraper", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Database configuration
 const databaseUrl = "articlescraper";
